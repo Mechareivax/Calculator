436e9e824a2fee88d260e149a24dbd8f95934433
Xavier Medrano <xmedr10@gmail.com>
2022-06-05 17:04:09 -0400

    start adding functionality to the buttons
    TODO: add an event listener to the equals button that will clear the
          previous operator and secondNum, while assigning firstNum the value of the solution

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let result = null;
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+const equalsButton = document.querySelector('#equals');
+const operandButton = document.querySelectorAll(".operand");
+
 function add (a,b) {
     return a+b;
 }
@@ -14,18 +23,79 @@ function divide (a,b) {
     return a/b;
 }
 
-function operate (a, operand, b) {
-    switch (operand) {
-        case addition:
-            return add(a,b);
-        case subtraction:
-            return subtract(a,b);
-        case multiplication:
-            return multiply(a,b);
-        case division:
-            return divide(a,b);
+function operate (a, currentOperator, b) {
+    let solution;
+    
+    switch (currentOperator) {
+        case 'addition':
+            solution = add(a,b);
+            break;
+        case 'subtraction':
+            solution = subtract(a,b);
+            break;
+        case 'multiplication':
+            solution = multiply(a,b);
+            break;
+        case 'division':
+            solution = divide(a,b);
+            break;
         default:
             console.log('An error has occurred');
             break;
     }
+
+    return solution;
 }
+
+function clear() {
+    // TODO: attach this to the clear button
+    firstNum, secondNum, operator, result = null;
+}
+
+function getButtonValue(button) {
+    //get the current button's value
+
+    if (firstNum === null) {
+        display.setAttribute('value', button.textContent);
+        firstNum = parseFloat(button.textContent);
+        console.log('firstNum is now ' + firstNum);
+    } else if (isOperator(button.textContent)) {
+        operator = button.id;
+        console.log('Current value is an operator: ' + operator);
+    } else if (secondNum === null){
+        display.setAttribute('value', button.textContent);
+        secondNum = parseFloat(button.textContent);
+        console.log('secondNum has been assigned to ' + secondNum);
+    } else {
+        console.log('an error has occurred');
+    }
+}
+
+function isOperator (value) {
+    // return true if selection is an operator
+    //TODO: reeavluate if this is needed now that we have a selector for the operand buttons
+    const validOperators = ['÷', '×', '-', '+'];
+    return validOperators.includes(value);
+}
+
+function main () {
+    // TODO: add an event listener to the equals button that will clear the
+    // previous operator and secondNum, while assigning firstNum the value of the solution
+    // maybe move that if statement below into that listener
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            
+            getButtonValue(e.target);
+
+            if(firstNum && operator && secondNum) {
+                result = operate(firstNum, operator, secondNum);
+                console.log(result);
+            }
+            
+        })
+    })
+
+
+}
+
+main()
